d9c0ab03c31fb4a31887bb1620b6e43a060ce911
Shreejal Maharjan <shreejal27@gmail.com>
2024-03-29 23:46:41 +0545

    made a seperate sidebar for admin and user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -114,9 +114,9 @@ export default function Login() {
       localStorage.setItem('userName', response.userName);
       localStorage.setItem('userType', response.userType);
       if (response.userType === "TellerPerson")
-        window.location.href = "/adminDashboard";
+        window.location.href = "/adminhomepage";
       else if (response.userType === "AccountHolder")
-        window.location.href = "/userDashboard";
+        window.location.href = "/userhomepage";
       else
         window.location.href = "/aaa";
     } else {
